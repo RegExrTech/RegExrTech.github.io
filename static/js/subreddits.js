e4c53815fd9b0fc7afd981296eedf0caad5b3568
Timothy Moore <mtimothy984@gmail.com>
2022-10-28 15:23:21 -0700

    Add lint rules

--- static/js/subreddits.js
+++ static/js/subreddits.js
@@ -1,26 +1,28 @@
 function Get(yourUrl) {
-	var Httpreq = new XMLHttpRequest();
-	Httpreq.open("GET",yourUrl,false);
-	Httpreq.send(null);
-	return Httpreq.responseText;
+  var Httpreq = new XMLHttpRequest();
+  Httpreq.open('GET', yourUrl, false);
+  Httpreq.send(null);
+  return Httpreq.responseText;
 }
 
 ///////////////////
 // ON LOAD BELOW //
 ///////////////////
 sublist = document.getElementById('sublist');
-const subreddits = JSON.parse(Get('https://www.reddit.com/r/UniversalScammerList/wiki/participating_subreddits.json')).data.content_md.split("\n\n");
+const subreddits = JSON.parse(
+  Get('https://www.reddit.com/r/UniversalScammerList/wiki/participating_subreddits.json')
+).data.content_md.split('\n\n');
 for (const subreddit of subreddits) {
-	const content = subreddit.split("* ")[1].split("\n")[0]
-	var linkText = document.createTextNode(content);
-	var a = document.createElement('a');
-	a.appendChild(linkText);
-	a.title = content;
-  	a.href = "https://www.reddit.com/" + content;
-	var li = document.createElement("li");
-	li.appendChild(a);
-	sublist.appendChild(li);
+  const content = subreddit.split('* ')[1].split('\n')[0];
+  var linkText = document.createTextNode(content);
+  var a = document.createElement('a');
+  a.appendChild(linkText);
+  a.title = content;
+  a.href = 'https://www.reddit.com/' + content;
+  var li = document.createElement('li');
+  li.appendChild(a);
+  sublist.appendChild(li);
 }
 
-sublist.style.visibility = "visible";
-document.getElementById('loadingmessage').style.visibility = "hidden";
+sublist.style.visibility = 'visible';
+document.getElementById('loadingmessage').style.visibility = 'hidden';
