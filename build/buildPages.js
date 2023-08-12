abc951024439babcb89028ec85092cbbce355bef
unknown <me@nathanielh.com>
2023-08-11 21:21:49 -0400

    Propaged changes from built files to the templates
    Propaged changes from built files in / to the templates in /build/
    
    Also added a warning comment to the generated files to avoid any issues in the future

--- build/buildPages.js
+++ build/buildPages.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 const pageWrap = fs.readFileSync('build/pageWrap.mustache').toString();
 
+//ignore the warning on the line following this one, it is a comment inserted into the generated pages. This file (buildPages.js) is safe to edit.
+const warningComment = "<!------------ Please don't edit this file: this is the result of an automated build script. Please edit the files in /build/ instead, and run the build script. ------------>"
+
 const views = [
   {
     title: 'About',
@@ -41,6 +44,7 @@ const views = [
 
 for (const view of views) {
   view.pageContent = fs.readFileSync('build/fragments/' + view.page).toString();
+  view.warningComment = warningComment;
   const output = Mustache.render(pageWrap, view);
   console.log(view.page);
   fs.writeFileSync('./' + view.page, output);
