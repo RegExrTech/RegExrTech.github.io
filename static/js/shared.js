599f135b8fa798b7728f0a9772e553f9209aeb55
devonwasson <devonwasson@gmail.com>
2024-12-05 10:55:00 -0500

    fix fetchAndSplit() to differentiate between 404 and CORS

--- static/js/shared.js
+++ static/js/shared.js
@@ -2,15 +2,24 @@
 
 function fetchAndSplit(url) {
   return fetch(url)
-    .then((res) => res.json())
+    .then((res) => {
+      hide('loadingMessage');
+      if (res.status == '404') {
+        throw new Error("HTTP status 404 for url " + url);
+      }
+      return res.json();
+    })
     .then((res) => res.data.content_md.split('\n'))
     .catch((error) => {
       console.error('fetchAndSplit saw error:', error);
-      document.getElementById(
-        'loadingMessage'
-      ).innerHTML = `Error loading data from Reddit: <i>"${error}"</i>.<br>
-            If you're using Firefox, try disabling enhanced tracking protection for this site using the little shield icon to the left of your URL bar.
-            (The USL is not tracking you, but Reddit gets detected as a tracker by Firefox).`;
+      if (!error.message.includes('404')) {
+        document.getElementById(
+          'loadingMessage'
+        ).innerHTML = `Error loading data from Reddit: <i>"${error}"</i>.<br>
+              If you're using Firefox, try disabling enhanced tracking protection for this site using the little shield icon to the left of your URL bar.
+              (The USL is not tracking you, but Reddit gets detected as a tracker by Firefox).`;
+        show('loadingMessage');
+      }
     });
 }
 
